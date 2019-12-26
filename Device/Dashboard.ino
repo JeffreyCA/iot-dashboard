f85db3ad74390ac4edb2b6190ff2315513c89d0d
JeffreyCA <jeffreyca16@gmail.com>
2019-12-25 19:56:14 -0500

    Support stock price and weather information using Azure Function

--- Device/Dashboard.ino
+++ Device/Dashboard.ino
@@ -5,108 +5,15 @@
 #include "arduinojson.h";
 #include "http_client.h";
 
+#include "api.h";
 #include "config.h";
+#include "splash.h";
 
-enum RATE_CHANGE { DECREASE, SAME, INCREASE };
+static const String endpoint = (String(FUNCTION_URL) + "?code=" + FUNCTION_KEY +
+                   "&currencypair=" + CURRENCY_PAIR + "&stock=" + STOCK_TICKER +
+                   "&location=" + WEATHER_LOCATION);
 
-static unsigned char AZURE_BMP[] = {
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   128, 192, 192, 224, 240, 56,  12,  192,
-    240, 224, 192, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   128, 224, 224, 224, 224,
-    128, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   192, 224, 248, 252, 254, 255,
-    255, 63,  15,  3,   64,  248, 254, 255, 255, 255, 255, 255, 252, 248, 224,
-    128, 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    128, 240, 252, 127, 31,  3,   3,   15,  127, 254, 240, 192, 0,   0,   0,
-    0,   8,   28,  28,  28,  28,  156, 220, 252, 124, 60,  28,  0,   0,   0,
-    252, 252, 248, 0,   0,   0,   0,   0,   0,   252, 252, 248, 0,   0,   0,
-    0,   252, 252, 252, 112, 56,  28,  28,  28,  0,   128, 224, 240, 248, 28,
-    28,  12,  12,  28,  248, 248, 240, 128, 0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   128, 224, 248,
-    252, 255, 255, 255, 255, 63,  15,  3,   0,   0,   0,   0,   0,   0,   3,
-    7,   15,  31,  63,  127, 255, 255, 255, 255, 255, 252, 240, 224, 128, 0,
-    0,   0,   0,   0,   0,   64,  112, 124, 127, 31,  7,   7,   7,   7,   7,
-    7,   7,   7,   31,  127, 126, 120, 64,  0,   96,  112, 120, 126, 127, 103,
-    99,  97,  96,  96,  96,  0,   0,   0,   63,  127, 127, 240, 224, 224, 96,
-    96,  56,  127, 127, 127, 0,   0,   0,   0,   127, 127, 127, 0,   0,   0,
-    0,   0,   0,   15,  63,  127, 123, 243, 227, 227, 227, 227, 99,  99,  35,
-    3,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   1,   1,   1,   1,   1,   1,   1,   0,   8,   8,
-    8,   8,   8,   12,  12,  12,  12,  12,  14,  14,  14,  14,  14,  15,  15,
-    15,  15,  15,  15,  15,  15,  15,  15,  12,  0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-    0,   0,   0,   0};
-
-static const char SSL_CA_PEM[] =
-    "-----BEGIN CERTIFICATE-----\n"
-    "MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ\n"
-    "RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD\n"
-    "VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX\n"
-    "DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y\n"
-    "ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy\n"
-    "VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr\n"
-    "mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr\n"
-    "IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK\n"
-    "mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu\n"
-    "XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy\n"
-    "dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye\n"
-    "jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1\n"
-    "BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3\n"
-    "DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92\n"
-    "9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx\n"
-    "jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0\n"
-    "Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz\n"
-    "ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS\n"
-    "R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp\n"
-    "-----END CERTIFICATE-----\n";
-
-static const char *FOREX_URL =
-    "https://free.currconv.com/api/v7/"
-    "convert?q=USD_CAD&compact=ultra&apiKey=" API_KEY;
-static const char *RATE_KEY = "USD_CAD";
+static const char *endpoint_url = endpoint.c_str();
 
 static bool has_wifi = false;
 static bool http_error = false;
@@ -116,20 +23,26 @@ static int led_countdown = 0;
 static int update_rate_countdown = 0;
 
 static float rate = 0.0;
+static float stock_price = 0.0;
+static const char *condition = "";
+static float temp = 0.0;
 
 static bool is_led_on = false;
 static bool is_refreshing = false;
 
+static int last_button_a_state = HIGH;
 static int last_button_b_state = HIGH;
-static RATE_CHANGE rate_change = SAME;
+
+static int current_view = 0;
 
 RGB_LED rgbLed;
 
+// Establish wifi connection.
 static void init_wifi() {
-  Screen.print(0, "Connecting...");
+  Screen.clean();
+  Screen.print(0, "Connecting to Wifi...", true);
 
   if (WiFi.begin() == WL_CONNECTED) {
-    IPAddress ip = WiFi.localIP();
     has_wifi = true;
   } else {
     has_wifi = false;
@@ -137,9 +50,12 @@ static void init_wifi() {
   }
 }
 
-static void update_rate() {
-  HTTPClient *httpClient = new HTTPClient(SSL_CA_PEM, HTTP_GET, FOREX_URL);
+static void update_info() {
+  const char *endpoint_url = endpoint.c_str();
+  HTTPClient *httpClient = new HTTPClient(SSL_CA_PEM, HTTP_GET, endpoint_url);
   const Http_Response *result = httpClient->send();
+
+  Serial.println(endpoint_url);
 
   if (result == NULL) {
     http_error = true;
@@ -148,45 +64,97 @@ static void update_rate() {
     update_rate_countdown = FAIL_REFRESH_INTERVAL;
   } else {
     const char *response = result->body;
-    StaticJsonDocument<32> doc;
+    StaticJsonDocument<256> doc;
     DeserializationError err = deserializeJson(doc, response);
 
     if (err) {
       http_error = true;
-      Serial.print(F("deserializeJson() failed with code "));
+      Serial.print(F("deserializeJson() failed with code"));
       Serial.println(err.c_str());
     } else {
-        http_error = false;
-      float new_rate = doc[RATE_KEY];
+      http_error = false;
+      is_led_on = true;
 
-      if (new_rate < rate) {
-        rate_change = DECREASE;
-        is_led_on = true;
-      } else if (new_rate == rate) {
-        rate_change = SAME;
-        is_led_on = false;
-      } else {
-        rate_change = INCREASE;
-        is_led_on = true;
-      }
+      float new_rate = doc["exchange_rate"];
+      float new_stock_price = doc["stock_price"];
+      const char *new_condition = doc["condition"];
+      float new_temp = doc["temp"];
 
       rate = new_rate;
+      stock_price = new_stock_price;
+      condition = new_condition;
+      temp = new_temp;
     }
 
     Serial.print("Rate: ");
     Serial.println(rate, 4);
 
-    led_countdown = LED_DURATION_SEC;
+    Serial.print("Stock price: ");
+    Serial.println(stock_price, 2);
+
+    Serial.print("Condition: ");
+    Serial.println(condition);
+
+    Serial.print("Temp: ");
+    Serial.println(temp, 1);
+    Serial.println();
+
+    led_countdown = LED_FLASH_INTERVAL;
     update_rate_countdown = REFRESH_INTERVAL;
   }
 
+  is_refreshing = false;
   delete httpClient;
 }
 
 static void show_rate() {
-  char rate_buffer[24];
-  sprintf(rate_buffer, "USD = %.4f CAD", rate);
-  Screen.print(1, rate_buffer);
+  char header_buffer[MAX_LINE_BUFFER_SIZE];
+  char subheader_buffer[MAX_LINE_BUFFER_SIZE];
+
+  sprintf(header_buffer, "%s Rate", CURRENCY_PAIR);
+  sprintf(subheader_buffer, "%s = %.4f %s", CURRENCY_1, rate, CURRENCY_2);
+
+  Screen.print(0, header_buffer);
+  Screen.print(1, subheader_buffer);
+}
+
+static void show_stock() {
+  char header_buffer[MAX_LINE_BUFFER_SIZE];
+  char subheader_buffer[12];
+
+  sprintf(header_buffer, "%s Price", STOCK_TICKER);
+  sprintf(subheader_buffer, "$%.2f", stock_price);
+
+  Screen.print(0, header_buffer);
+  Screen.print(1, subheader_buffer);
+}
+
+static void show_weather() {
+  char subheader_buffer[8];
+  sprintf(subheader_buffer, "%.1f C", temp);
+
+  Screen.print(0, WEATHER_LOCATION);
+  Screen.print(1, subheader_buffer);
+}
+
+static void show_view() {
+  if (current_view == 0) {
+    show_rate();
+  } else if (current_view == 1) {
+    show_stock();
+  } else if (current_view == 2) {
+    show_weather();
+  }
+}
+
+static void update_and_show_view() {
+  update_info();
+
+  if (http_error) {
+    Screen.print(1, "Error");
+  } else {
+    show_view();
+  }
 }
 
 static void show_countdown() {
@@ -197,7 +165,7 @@ static void show_countdown() {
 
 void setup() {
   Screen.init();
-  Screen.draw(0, 0, 128, 8, AZURE_BMP);
+  Screen.draw(0, 0, 128, 8, SPLASH_BMP);
   delay(SPLASH_DURATION_MS);
   Screen.clean();
 
@@ -211,43 +179,55 @@ void setup() {
 
   if (has_wifi) {
     Screen.clean();
-    Screen.print(0, "USD/CAD Rate");
-    update_rate();
-    show_rate();
+    update_info();
+
+    if (http_error) {
+      Screen.print(1, "Error");
+    } else {
+      show_view();
+    }
   }
 }
 
 void loop() {
   if (!has_wifi) {
-    Serial.println("No Wi-Fi");
+    has_wifi = false;
+    init_wifi();
     return;
   }
 
   unsigned long current_ms = millis();
 
+  if (last_button_a_state == HIGH && digitalRead(USER_BUTTON_A) == LOW) {
+    last_button_a_state = LOW;
+
+    current_view = (current_view + 1) % 3;
+
+    Serial.print("Switching to view: ");
+    Serial.println(current_view);
+
+    rgbLed.turnOff();
+    show_view();
+  } else if (last_button_a_state == LOW && digitalRead(USER_BUTTON_A) == HIGH) {
+    last_button_a_state = HIGH;
+  }
+
   if (last_button_b_state == HIGH && digitalRead(USER_BUTTON_B) == LOW) {
     last_button_b_state = LOW;
 
-    Serial.println("Force refresh...");
-    Screen.print(3, "Refreshing...");
-    rgbLed.turnOff();
-
-    update_rate();
+    if (!is_refreshing) {
+      is_refreshing = true;
+      Serial.println("Force refresh...");
+      Screen.print(3, "Refreshing...");
+      rgbLed.turnOff();
+      update_and_show_view();
+    }
   } else if (last_button_b_state == LOW && digitalRead(USER_BUTTON_B) == HIGH) {
     last_button_b_state = HIGH;
   }
 
   if (is_led_on) {
-    switch (rate_change) {
-      case DECREASE:
-        rgbLed.setColor(255, 0, 0);
-        break;
-      case INCREASE:
-        rgbLed.setColor(0, 255, 0);
-        break;
-      default:
-        break;
-    }
+    rgbLed.setColor(0, 0, 255);
   } else {
     rgbLed.turnOff();
   }
@@ -259,14 +239,11 @@ void loop() {
     led_countdown--;
 
     if (update_rate_countdown < 0) {
-      Screen.print(3, "Refreshing...");
-      rgbLed.turnOff();
-      update_rate();
-
-      if (http_error) {
-        Screen.print(1, "Error");
-      } else {
-        show_rate();
+      if (!is_refreshing) {
+        is_refreshing = true;
+        Screen.print(3, "Refreshing...");
+        rgbLed.turnOff();
+        update_and_show_view();
       }
     }
 
--- Device/config.h
+++ Device/config.h
@@ -1,11 +1,22 @@
-#define API_KEY "{currencyconverterapi.com API key}"
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#define SPLASH_DURATION_MS 3000
+// User-configurable values
+#define CURRENCY_1 "USD"
+#define CURRENCY_2 "CAD"
+#define STOCK_TICKER "MSFT"
+#define WEATHER_LOCATION "Toronto"
 
-#define LED_DURATION_SEC 1
+// Other constants
+#define CURRENCY_PAIR CURRENCY_1 "/" CURRENCY_2
 
+#define MAX_LINE_BUFFER_SIZE 17
+#define MAX_BUFFER_SIZE 65
+
+#define SPLASH_DURATION_MS 1500
 #define UPDATE_INTERVAL_MS 1000
-
+#define LED_FLASH_INTERVAL 1
 #define FAIL_REFRESH_INTERVAL 10
-
 #define REFRESH_INTERVAL 120
+
+#endif // CONFIG_H
